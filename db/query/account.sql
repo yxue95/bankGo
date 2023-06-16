017bda73a0d7bde8bd472e1f7958737b67ace5a2
yxue95 <yxue95@gatech.edu>
2023-06-15 20:08:00 -0400

    golang crud and unit test

--- db/query/account.sql
+++ db/query/account.sql
@@ -13,7 +13,7 @@ SELECT * FROM accounts WHERE id = $1 LIMIT 1;
 -- name: ListAccounts :many
 SELECT * FROM accounts ORDER BY id LIMIT $1 OFFSET $2;
 
--- name: UpdateAccount :exec
+-- name: UpdateAccount :one
 UPDATE accounts SET balance = $2 WHERE id = $1
 RETURNING *;
 
